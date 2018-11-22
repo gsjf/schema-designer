ca094b49b2b2ac5a39bd60b72e4d93a1d8a3950c
zhanglun <zhanglun@guoshujinfu.com>
2018-11-22 17:10:22 +0800

    :star: add table delete confirm

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,7 @@
  */
 import noop from 'lodash/noop';
 import React, { PureComponent } from 'react';
+import Modal from 'react-bootstrap/lib/Modal';
 import Columns from '../containers/Columns';
 import type { TableType } from '../utils/flowtypes';
 
@@ -17,26 +18,39 @@ type Props = {
     onToggleColumnModal: (id: string) => void
 };
 
-class Table extends PureComponent<Props> {
+type State = {
+    showConfirm: boolean
+};
+
+class Table extends PureComponent<Props, State> {
+    state = {
+        showConfirm: false
+    };
+
     editTable = () => {
         const { data, onEditTable } = this.props;
         onEditTable(data);
-    }
+    };
 
     removeTable = () => {
         const { data, onRemoveTable } = this.props;
         onRemoveTable(data.id);
-    }
+    };
 
     toggleColumnModal = () => {
         const { data, onToggleColumnModal } = this.props;
         onToggleColumnModal(data.id);
-    }
+    };
+
+    toggleConfirm = () => {
+        const { showConfirm } = this.state;
+        this.setState({ showConfirm: !showConfirm });
+    };
 
     render() {
         console.log('Table rendering'); // eslint-disable-line no-console
         const { data, position } = this.props;
-
+        const { showConfirm } = this.state;
         return (
             <div
                 className='db-table draggable no-select'
@@ -58,12 +72,51 @@ class Table extends PureComponent<Props> {
                             onKeyPress={ noop }
                         >
                         </span>
+
                         <span
                             className='fa fa-remove'
-                            onClick={ this.removeTable }
+                            onClick={ this.toggleConfirm }
                             onKeyPress={ noop }
                         >
                         </span>
+
+                        <Modal
+                            show={ showConfirm }
+                            onHide={ this.toggleConfirm }
+                            dialogClassName='modal-sm'
+                        >
+                            <Modal.Header>
+                                <button type='button' className='close' onClick={ this.toggleConfirm }>
+                                    <span>&times;</span>
+                                </button>
+                                <Modal.Title>
+                                    Delete Confirm
+                                </Modal.Title>
+                            </Modal.Header>
+
+                            <Modal.Body>
+                                <h5 className='modal-body'>
+                                    Are you sure?
+                                </h5>
+                            </Modal.Body>
+
+                            <Modal.Footer className='modal-footer text-right'>
+                                <button
+                                    type='button'
+                                    className='btn btn-primary'
+                                    onClick={ this.removeTable }
+                                    onKeyPress={ noop }
+                                > Delete
+                                </button>
+                                <button
+                                    type='button'
+                                    className='btn btn-primary'
+                                    onClick={ this.toggleConfirm }
+                                >Cancel
+                                </button>
+                            </Modal.Footer>
+                        </Modal>
+
                     </div>
                 </div>
 
